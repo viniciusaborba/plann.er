64edf2d5dc568c96c149723fcf96d0a720e6469f
vinicius borba <viniboal28@gmail.com>
2024-10-20 21:24:22 -0300

    feat: add initial validator to api

--- gen.go
+++ gen.go
@@ -1,5 +1,6 @@
 package main
 
+//go:generate goapi-gen --package=planner --out ./internal/api/spec/planner.gen.spec.go ./internal/api/spec/planner.spec.json
 //go:generate ./migrate.sh
 //go:generate sqlc generate -f ./internal/pgstore/sqlc.yaml
 
--- internal/api/api.go
+++ internal/api/api.go
@@ -2,14 +2,16 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
-	"plann.er/internal/api/spec"
+	planner "plann.er/internal/api/spec"
 	"plann.er/internal/pgstore"
 )
 
@@ -21,88 +23,98 @@ type store interface {
 type API struct {
 	store store
 	logger *zap.Logger
+	validator *validator.Validate
 }
 
 func NewApi(pool *pgxpool.Pool, logger *zap.Logger) API {
-	return API{pgstore.New(pool), logger}
+	validator := validator.New(validator.WithRequiredStructEnabled())
+	return API{pgstore.New(pool), logger, validator}
 }
 
 // Confirms a participant on a trip.
 // (PATCH /participants/{participantId}/confirm)
-func (api *API) PatchParticipantsParticipantIDConfirm(w http.ResponseWriter, r *http.Request, participantID string) *spec.Response {
+func (api *API) PatchParticipantsParticipantIDConfirm(w http.ResponseWriter, r *http.Request, participantID string) *planner.Response {
 	id, err := uuid.Parse(participantID)
 	if err != nil {
-		return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{Message: "invalid uuid"})
+		return planner.PatchParticipantsParticipantIDConfirmJSON400Response(planner.Error{Message: "invalid uuid"})
 	}
 
 	participant, err := api.store.GetParticipant(r.Context(), id)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
-			return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{Message: "participant not found"})
+			return planner.PatchParticipantsParticipantIDConfirmJSON400Response(planner.Error{Message: "participant not found"})
 		}
 		api.logger.Error("failed to get participant", zap.Error(err), zap.String("participant_id", participantID))
-		return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{Message: "something went wrong"})
+		return planner.PatchParticipantsParticipantIDConfirmJSON400Response(planner.Error{Message: "something went wrong"})
 	}
 
 	if participant.IsConfirmed {
-		return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{Message: "participant is already confirmed"})
+		return planner.PatchParticipantsParticipantIDConfirmJSON400Response(planner.Error{Message: "participant is already confirmed"})
 	}
 
 	if err := api.store.ConfirmParticipant(r.Context(), id); err != nil {
 		api.logger.Error("failed to confirm participant", zap.Error(err), zap.String("participant_id", participantID))
-		return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{Message: "something went wrong"})
+		return planner.PatchParticipantsParticipantIDConfirmJSON400Response(planner.Error{Message: "something went wrong"})
 	}
 
-	return spec.PatchParticipantsParticipantIDConfirmJSON204Response(nil)
+	return planner.PatchParticipantsParticipantIDConfirmJSON204Response(nil)
 }
 
 // Create a new trip
 // (POST /trips)
-func (API) PostTrips(w http.ResponseWriter, r *http.Request) *spec.Response {
-	panic("not implemented") // TODO: Implement
+func (api *API) PostTrips(w http.ResponseWriter, r *http.Request) *planner.Response {
+	var body planner.CreateTripRequest
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		return planner.PostTripsJSON400Response(planner.Error{Message: "invalid JSON"})
+	}
+	
+	if err := api.validator.Struct(body); err != nil {
+		return planner.PostTripsJSON400Response(planner.Error{Message: "invalid input"})
+	}
+
 }
 // Get a trip details.
 // (GET /trips/{tripId})
-func (API) GetTripsTripID(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+func (API) GetTripsTripID(w http.ResponseWriter, r *http.Request, tripID string) *planner.Response {
 	panic("not implemented") // TODO: Implement
 }
 // Update a trip.
 // (PUT /trips/{tripId})
-func (API) PutTripsTripID(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+func (API) PutTripsTripID(w http.ResponseWriter, r *http.Request, tripID string) *planner.Response {
 	panic("not implemented") // TODO: Implement
 }
 // Get a trip activities.
 // (GET /trips/{tripId}/activities)
-func (API) GetTripsTripIDActivities(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+func (API) GetTripsTripIDActivities(w http.ResponseWriter, r *http.Request, tripID string) *planner.Response {
 	panic("not implemented") // TODO: Implement
 }
 // Create a trip activity.
 // (POST /trips/{tripId}/activities)
-func (API) PostTripsTripIDActivities(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+func (API) PostTripsTripIDActivities(w http.ResponseWriter, r *http.Request, tripID string) *planner.Response {
 	panic("not implemented") // TODO: Implement
 }
 // Confirm a trip and send e-mail invitations.
 // (GET /trips/{tripId}/confirm)
-func (API) GetTripsTripIDConfirm(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+func (API) GetTripsTripIDConfirm(w http.ResponseWriter, r *http.Request, tripID string) *planner.Response {
 	panic("not implemented") // TODO: Implement
 }
 // Invite someone to the trip.
 // (POST /trips/{tripId}/invites)
-func (API) PostTripsTripIDInvites(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+func (API) PostTripsTripIDInvites(w http.ResponseWriter, r *http.Request, tripID string) *planner.Response {
 	panic("not implemented") // TODO: Implement
 }
 // Get a trip links.
 // (GET /trips/{tripId}/links)
-func (API) GetTripsTripIDLinks(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+func (API) GetTripsTripIDLinks(w http.ResponseWriter, r *http.Request, tripID string) *planner.Response {
 	panic("not implemented") // TODO: Implement
 }
 // Create a trip link.
 // (POST /trips/{tripId}/links)
-func (API) PostTripsTripIDLinks(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+func (API) PostTripsTripIDLinks(w http.ResponseWriter, r *http.Request, tripID string) *planner.Response {
 	panic("not implemented") // TODO: Implement
 }
 // Get a trip participants.
 // (GET /trips/{tripId}/participants)
-func (API) GetTripsTripIDParticipants(w http.ResponseWriter, r *http.Request, tripID string) *spec.Response {
+func (API) GetTripsTripIDParticipants(w http.ResponseWriter, r *http.Request, tripID string) *planner.Response {
 	panic("not implemented") // TODO: Implement
 }
